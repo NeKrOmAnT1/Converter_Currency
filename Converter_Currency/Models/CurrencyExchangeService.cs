e9bc772ac4195ad44090a91b37b5b35b48fa7ee6
Pro <Pro@DESKTOP-58CS0P2>
2023-09-07 14:52:03 +0300

    Добавьте файлы проекта.

---
+++ Converter_Currency/Models/CurrencyExchangeService.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter_Currency.Models
+{
+    class CurrencyExchangeService
+    {
+        private const string ApiUrl = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+        public async Task<List<string>> GetCurrencyListAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(ApiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string data = await response.Content.ReadAsStringAsync();
+                        var currencyData = JsonConvert.DeserializeObject<CurrencyData>(data);
+
+                        List<string> currencies = new List<string>(currencyData.Valute.Keys);
+                        return currencies;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
+        }
+            
+
+    }
+
+    public class CurrencyData
+    {
+        public Dictionary <string, CurrencyInfo> Valute { get; set; }
+    }
+    public class CurrencyInfo
+    {
+        public string Name { get; set;}
+        public decimal Value { get; set;}
+
+    }
+}
---
+++ Converter_Currency/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Converter_Currency.Models;
+using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter_Currency.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private CurrencyExchangeService _currencyService = new CurrencyExchangeService();
+
+        public ObservableCollection<string> Currencies { get; set; }
+        public decimal ResultCurrency { get; set; }
+        public decimal Original { get; set; }
+        public string SelectedCurrency { get; set; }
+        public string SelectedTwoCurrency { get; set; }
+
+
+        public MainWindowViewModel()
+        {
+            LoadCurrencyList();
+
+        }
+
+
+        private async void LoadCurrencyList()
+        {
+            List<string> currencyList = await _currencyService.GetCurrencyListAsync();
+
+            if (currencyList != null)
+            {
+                Currencies = new ObservableCollection<string>(currencyList);
+                SelectedCurrency = currencyList[0];
+            }
+            else
+            {
+            }
+        }
+    }
+}
